bbf62b36895c3f5ef9cd8477ef592f56a22007e9
Victor Felder <victor@draft.li>
2018-07-31 15:51:35 +0200

    Add docker postgraphile + initial migrations

---
+++ docker-app-dev/entrypoint.postgraphile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+postgraphile \
+  -n 0.0.0.0 \
+  --connection postgres://ontology_editor_postgraphile:password_placeholder@db:5432/$POSTGRESQL_DATABASE \
+  --secret $TOKENSECRET \
+  --schema ontology_editor \
+  --default-role ontology_editor_anonymous \
+  --token ontology_editor.jwt_token
