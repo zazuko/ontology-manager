acfc8ca649a731851c3fe1ce0718df87df17245e
Victor Felder <victor@draft.li>
2018-08-14 13:57:01 +0200

    Add instructions to README

--- docker-app-dev/entrypoint.postgraphile.sh
+++ docker-app-dev/entrypoint.postgraphile.sh
@@ -3,7 +3,7 @@
 DEBUG="postgraphile:*" postgraphile \
   -n 0.0.0.0 \
   --show-error-stack \
-  --extended-errors severity,code,detail,hint,positon,internalPosition,internalQuery,where,schema,table,column,dataType,constraint,file,line,routine \
+  --extended-errors severity,code,detail,hint,position,internalPosition,internalQuery,where,schema,table,column,dataType,constraint,file,line,routine \
   --connection postgres://ontology_editor_postgraphile:password_placeholder@db:5432/$POSTGRESQL_DATABASE \
   --secret $POSTGRAPHILE_TOKEN_SECRET \
   --schema ontology_editor \
