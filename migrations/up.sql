3b3e848b56853f8e917416a283d0b518cd8498b4
Victor Felder <victor@draft.li>
2018-08-15 14:33:51 +0200

    Add admin mgmt; assign objects to current user

--- migrations/up.sql
+++ migrations/up.sql
@@ -9,13 +9,15 @@ create schema ontology_editor_private;
 create table ontology_editor.person (
   id               serial primary key,
   name             text not null check (char_length(name) < 80),
+  is_admin         boolean not null default false,
   about            text,
   created_at       timestamp default now()
 );
 
-comment on table ontology_editor.person is 'A user of the ontology editor.';
+comment on table ontology_editor.person is '@omit all\nA user of the ontology editor.';
 comment on column ontology_editor.person.id is 'The primary unique identifier for the person.';
 comment on column ontology_editor.person.name is 'The person’s first name.';
+comment on column ontology_editor.person.is_admin is 'If true, the user has elevated privileges.';
 comment on column ontology_editor.person.about is 'A short description about the user, written by the user.';
 comment on column ontology_editor.person.created_at is 'The time this person was created.';
 
@@ -41,9 +43,29 @@ create type ontology_editor.thread_type as enum (
   'proposal'
 );
 
+create function ontology_editor.current_person() returns ontology_editor.person as $$
+  select *
+  from ontology_editor.person
+  where id = current_setting('jwt.claims.person_id')::integer
+$$ language sql stable;
+comment on function ontology_editor.current_person() is 'Gets the person who was identified by our JWT.';
+
+create function ontology_editor.current_person_id() returns int as $$
+  select nullif(current_setting('jwt.claims.person_id', true), '')::int;
+$$ language sql stable set search_path from current;
+comment on function ontology_editor.current_person_id() is 'Gets the person who was identified by our JWT.';
+
+-- We're using exists here because it guarantees true/false rather than true/false/null
+create function ontology_editor.current_person_is_admin() returns bool as $$
+  select exists(
+    select 1 from ontology_editor.person where id = ontology_editor.current_person_id() and is_admin = true
+	);
+$$ language sql stable set search_path from current;
+comment on function ontology_editor.current_person_is_admin() is E'@omit\nHandy method to determine if the current user is an admin, for use in RLS policies, etc; in GraphQL should use `currentUser{isAdmin}` instead.';
+
 create table ontology_editor.thread (
   id               serial primary key,
-  author_id        integer not null references ontology_editor.person(id),
+  author_id        integer not null default ontology_editor.current_person_id() references ontology_editor.person(id),
   hat_id           integer default null references ontology_editor.hat(id),
   external_id      integer default null ,
   headline         text not null check (char_length(headline) < 280),
@@ -66,7 +88,7 @@ comment on column ontology_editor.thread.created_at is 'The time this thread was
 
 create table ontology_editor.message (
   id               serial primary key,
-  author_id        integer not null references ontology_editor.person(id),
+  author_id        integer not null default ontology_editor.current_person_id() references ontology_editor.person(id),
   thread_id        integer not null references ontology_editor.thread(id),
   hat_id           integer default null references ontology_editor.hat(id),
   body             text,
@@ -119,6 +141,20 @@ comment on function ontology_editor.search_messages(text) is 'Returns threads co
 alter table ontology_editor.person add column updated_at timestamp default now();
 alter table ontology_editor.thread add column updated_at timestamp default now();
 alter table ontology_editor.message add column updated_at timestamp default now();
+
+create function ontology_editor_private.tg_persons__make_first_person_admin() returns trigger as $$
+begin
+  if not exists(select 1 from ontology_editor.person) then
+    NEW.is_admin = true;
+  end if;
+  return NEW;
+end;
+$$ language plpgsql volatile set search_path from current;
+
+create trigger _make_first_person_admin
+  before insert on ontology_editor.person
+  for each row
+  execute procedure ontology_editor_private.tg_persons__make_first_person_admin();
 
 create function ontology_editor_private.set_updated_at() returns trigger as $$
 begin
@@ -246,14 +282,6 @@ $$ language plpgsql strict security definer;
 
 comment on function ontology_editor.authenticate(text, integer) is 'Creates a JWT token that will securely identify a person and give them certain permissions.';
 
-create function ontology_editor.current_person() returns ontology_editor.person as $$
-  select *
-  from ontology_editor.person
-  where id = current_setting('jwt.claims.person_id')::integer
-$$ language sql stable;
-
-comment on function ontology_editor.current_person() is 'Gets the person who was identified by our JWT.';
-
 grant usage on schema ontology_editor to ontology_editor_anonymous, ontology_editor_person;
 
 grant select on table ontology_editor.person to ontology_editor_anonymous, ontology_editor_person;
@@ -299,18 +327,18 @@ create policy select_message on ontology_editor.message for select
 create policy update_message on ontology_editor.message for update to ontology_editor_person
   using (author_id = current_setting('jwt.claims.person_id')::integer);
 create policy delete_message on ontology_editor.message for delete to ontology_editor_person
-  using (author_id = current_setting('jwt.claims.person_id')::integer);
+  using (author_id = current_setting('jwt.claims.person_id')::integer or ontology_editor.current_person_is_admin());
 
 -- CRUD
 -- thread
-create policy insert_message on ontology_editor.thread for insert to ontology_editor_person
+create policy insert_thread on ontology_editor.thread for insert to ontology_editor_person
   with check (author_id = current_setting('jwt.claims.person_id')::integer);
 create policy select_thread on ontology_editor.thread for select
   using (true);
 create policy update_thread on ontology_editor.thread for update to ontology_editor_person
-  using (author_id = current_setting('jwt.claims.person_id')::integer);
+  using (author_id = current_setting('jwt.claims.person_id')::integer or ontology_editor.current_person_is_admin());
 create policy delete_thread on ontology_editor.thread for delete to ontology_editor_person
-  using (author_id = current_setting('jwt.claims.person_id')::integer);
+  using (author_id = current_setting('jwt.claims.person_id')::integer or ontology_editor.current_person_is_admin());
 
 -- R
 -- hat
@@ -319,97 +347,12 @@ create policy select_hat on ontology_editor.hat for select
 
 -- CRD hat_person
 create policy insert_hat_person on ontology_editor.hat_person for insert to ontology_editor_person
-  with check (person_id = current_setting('jwt.claims.person_id')::integer);
+  with check (ontology_editor.current_person_is_admin());
 create policy select_hat_person on ontology_editor.hat_person for select
   using (true);
 create policy delete_hat_person on ontology_editor.hat_person for delete to ontology_editor_person
-  using (person_id = current_setting('jwt.claims.person_id')::integer);
+  using (ontology_editor.current_person_is_admin());
 
 commit;
 
 -- https://github.com/graphile/postgraphile/issues/91#issuecomment-308664225
-
--- BEGIN;
---
--- CREATE TEMPORARY TABLE plurals("from" text, "to" text);
--- -- Insert to the `plurals` table as appropriate to handle any irregular pluralisations in the generated function name.
--- -- INSERT INTO plurals VALUES ...;
---
--- CREATE FUNCTION ontology_editor.make_many_to_many(
---     many_schema text, many_table text, many_column_1 text, many_column_2 text,
---     schema_1 text, table_1 text, pk_1 text,
---     schema_2 text, table_2 text, pk_2 text)
--- RETURNS void AS $$
--- DECLARE
---     plural_2 text;
--- BEGIN
---     SELECT "to" FROM plurals WHERE "from" = table_2 INTO plural_2;
---     IF plural_2 IS NULL THEN
---         plural_2 := table_2 || 's';
---     END IF;
---
---     EXECUTE format('CREATE OR REPLACE FUNCTION %I.%I(a %I.%I) ', schema_1, format('%s_%s', table_1, plural_2), schema_1, table_1)
---          || format('RETURNS setof %I.%I AS $f$ ', schema_2, table_2)
---          || format('SELECT %I.%I.* FROM %I.%I ', schema_2, table_2, schema_2, table_2)
---          || format('INNER JOIN %I.%I ON %I.%I.%I = %I.%I.%I ', many_schema, many_table, schema_2, table_2, pk_2, many_schema, many_table, many_column_2)
---          || format('WHERE %I.%I.%I = a.%I; ', many_schema, many_table, many_column_1, pk_1)
---          || '$f$ LANGUAGE SQL STABLE';
--- END $$ LANGUAGE plpgsql;
---
--- CREATE FUNCTION ontology_editor.add_many_to_many(schema_name text, table_name text)
--- RETURNS void AS $$
--- DECLARE
---     fk1 RECORD;
---     fk2 RECORD;
---     count integer;
--- BEGIN
---     CREATE TEMPORARY TABLE fks AS
---         SELECT kcu.column_name,
---             ccu.table_schema foreign_table_schema,
---             ccu.table_name foreign_table_name,
---             ccu.column_name foreign_column_name
---         FROM information_schema.table_constraints tc
---         INNER JOIN information_schema.key_column_usage kcu
---             ON tc.constraint_name = kcu.constraint_name
---             AND tc.table_schema = kcu.table_schema
---         INNER JOIN information_schema.key_column_usage kcu2
---             ON kcu2.column_name = kcu.column_name
---             AND kcu2.table_name = kcu.table_name
---             AND kcu2.table_schema = kcu.table_schema
---         INNER JOIN information_schema.table_constraints tc2
---             ON tc2.constraint_name = kcu2.constraint_name
---             AND tc2.table_schema = kcu2.table_schema
---         INNER JOIN information_schema.constraint_column_usage ccu
---             ON ccu.constraint_name = tc2.constraint_name
---             AND ccu.table_schema = tc2.table_schema
---         WHERE tc.constraint_type = 'PRIMARY KEY'
---             AND tc.table_schema = add_many_to_many.schema_name
---             AND tc.table_name = add_many_to_many.table_name
---             AND tc2.constraint_type = 'FOREIGN KEY';
---
---     SELECT COUNT(*) FROM fks INTO count;
---
---     IF count = 2 THEN
---         SELECT * FROM fks LIMIT 1 INTO fk1;
---         SELECT * FROM fks OFFSET 1 LIMIT 1 INTO fk2;
---
---         EXECUTE ontology_editor.make_many_to_many(
---             schema_name, table_name,
---             fk1.column_name, fk2.column_name,
---             fk1.foreign_table_schema, fk1.foreign_table_name, fk1.foreign_column_name,
---             fk2.foreign_table_schema, fk2.foreign_table_name, fk2.foreign_column_name);
---         EXECUTE ontology_editor.make_many_to_many(
---             schema_name, table_name,
---             fk2.column_name, fk1.column_name,
---             fk2.foreign_table_schema, fk2.foreign_table_name, fk2.foreign_column_name,
---             fk1.foreign_table_schema, fk1.foreign_table_name, fk1.foreign_column_name);
---     END IF;
---     DROP TABLE fks;
--- END $$ LANGUAGE plpgsql;
---
--- SELECT ontology_editor.add_many_to_many(table_schema, table_name) FROM information_schema.tables WHERE table_schema = 'ontology_editor';
---
--- DROP FUNCTION ontology_editor.make_many_to_many(text, text, text, text, text, text, text, text, text, text);
--- DROP FUNCTION ontology_editor.add_many_to_many(text, text);
--- DROP TABLE plurals;
--- COMMIT;
