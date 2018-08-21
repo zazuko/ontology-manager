9ab6397132cab0a69e3f116266c0fbad9a06b94b
Victor Felder <victor@draft.li>
2018-08-21 21:38:33 +0200

    Add Github GraphQL API client

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 
+const config = require(require.resolve('../nuxt.config.js')).ontology
+
 const app = express()
 
 app.use(bodyParser.json())
 
-const api = require('./api')
+let api
+if (config.github) {
+  api = require('./github')(config.github)
+} else if (config.gitlab) {
+  api = require('./gitlab')(config.gitlab)
+} else {
+  throw new Error('No forge API configured or configured forge API not found.')
+}
 app.use('/', api)
 
 module.exports = { path: '/api', handler: app }
