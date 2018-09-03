4f628b6f78c7f482ee87191b853b852b8da533bb
Victor Felder <victor@draft.li>
2018-09-03 23:52:03 +0200

    Build and display structure tree

---
+++ components/Structure.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    :class="{
+      'is-ancestor': Object.keys(parent).length === 0,
+      [color]: true
+    }"
+    class="tile notification">
+    <div
+      :class="{
+        'is-child': Boolean(Object.keys(parent).length),
+        'is-parent': true || Boolean(obj.children && obj.children.length)
+      }"
+      class="tile is-vertical">
+
+      <p class="title">
+        <nuxt-link
+          :to="{ path: obj.path, params: {} }">
+          {{ name }}
+        </nuxt-link>
+      </p>
+
+      <structure
+        v-for="child in obj.children"
+        :key="child.lvl"
+        :obj="child"
+        :parent="obj"
+        :name="child.lvl"
+        :depth="depth + 1"
+        class="is-child"/>
+    </div>
+  </div>
+</template>
+
+<script>
+const depths = [
+  'is-warning',
+  'is-danger',
+  'is-primary',
+  'is-info'
+]
+
+export default {
+  name: 'Structure',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    obj: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    },
+    parent: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    },
+    depth: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      color: depths[this.depth]
+    }
+  }
+}
+</script>
