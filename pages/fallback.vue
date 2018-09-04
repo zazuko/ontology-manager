ead94c563d460037310f2b61bfbfefa0cd1e6d92
Victor Felder <victor@draft.li>
2018-09-04 16:01:31 +0200

    Fix rdf data loading

--- pages/fallback.vue
+++ pages/fallback.vue
@@ -1,17 +1,82 @@
 <template>
-  <section class="container">
+  <div>
+
+    <h1>{{ _iri }}</h1>
+
     <script
+      v-if="_jsonld"
       id="data"
       type="application/ld+json"
-      v-html="jsonld" />
-    <h1>yeah! {{ iri }}</h1>
-  </section>
+      v-html="_jsonld" />
+
+  </div>
 </template>
 
 <script>
 import rdf from 'rdf-ext'
 import JsonLdSerializer from 'rdf-serializer-jsonld'
 import Readable from 'readable-stream'
+import Structure from '~/components/Structure'
+const datasetBaseUrl = require('~/trifid/trifid.config.json').datasetBaseUrl
+
+export default {
+  components: {
+    Structure
+  },
+  async asyncData (context) {
+    if (process.server) {
+      const data = find(context.req.ontology, context.req.structure, context.iri)
+      if (data) {
+        return data
+      }
+
+      // nothing found so far, send 404
+      context.error({statusCode: 404, message: 'Not Found'})
+    }
+  },
+  data () {
+    if (typeof window !== 'undefined' && window.ontology) {
+      const data = find(window.ontology, window.structure, this._iri)
+      if (data) {
+        return data
+      }
+    }
+    return {}
+  },
+  computed: {
+    tree () {
+      return this.$store.state.graph.structureTree[this.iri]
+    },
+    _iri () {
+      if (this.iri) return this.iri
+      const params = this.$route.params
+      return datasetBaseUrl + [params.p1, params.p2, params.p3, params.p4].filter(Boolean).join('/')
+    },
+    _jsonld () {
+      if (this.jsonld) return this.jsonld
+      return ''
+    }
+  },
+  validate ({ params, store }) {
+    const ontology = store.state.graph.ontology
+    const structureTree = store.state.graph.structureTree
+    const iri = datasetBaseUrl + [params.p1, params.p2, params.p3, params.p4].filter(Boolean).join('/')
+    if (typeof ontology.find === 'function') {
+      if (
+        ontology.find(({subject}) => subject.termType === 'NamedNode' && subject.value === iri) ||
+        structureTree.hasOwnProperty(iri)
+      ) {
+        return true
+      }
+    } else if (
+      ontology.match(rdf.namedNode(iri)).length ||
+      structureTree.hasOwnProperty(iri)
+    ) {
+      return true
+    }
+    return false
+  }
+}
 
 function quadsToReadable (quads) {
   const readable = new Readable()
@@ -23,36 +88,38 @@ function quadsToReadable (quads) {
   return readable
 }
 
-export default {
-  async asyncData (context) {
-    if (process.server) {
-      const dataset = context.req.ontology
-      const matched = dataset.match(rdf.namedNode(context.iri))
-      if (!matched.length) {
-        context.error({statusCode: 404, message: 'Not Found'})
-        return
-      }
+async function find (ontology, structure, iri) {
+  // match the URL with the ontology dataset
+  let matched = ontology.match(rdf.namedNode(iri))
+  if (matched.length) {
+    // if found, serialize to JSONLD to hide it into the HTML
+    const jsonLdSerializer = new JsonLdSerializer({
+      process: [{flatten: true}, {compact: true}, {outputFormat: 'string'}]
+    })
+    const stream = jsonLdSerializer.import(quadsToReadable(matched))
 
-      const jsonLdSerializer = new JsonLdSerializer({
-        process: [{flatten: true}, {compact: true}, {outputFormat: 'string'}]
-      })
-      const stream = jsonLdSerializer.import(quadsToReadable(matched))
-
-      let result
-      stream.on('data', (data) => {
-        result = data
-      })
-      await rdf.waitFor(stream)
-
-      return {
-        iri: context.iri,
-        jsonld: result
-      }
-    }
+    let result
+    stream.on('data', (data) => {
+      result = data
+    })
+    await rdf.waitFor(stream)
 
     return {
-      iri: context.iri
+      iri,
+      jsonld: result
     }
   }
+
+  // match the URL with the structure dataset
+  matched = structure.match(rdf.namedNode(iri))
+  if (matched.length) {
+    return {
+      iri,
+      jsonld: ''
+    }
+  }
+
+  return false
 }
+
 </script>
--- store/index.js
+++ store/index.js
@@ -7,39 +7,6 @@ export const actions = {
     if (req) {
       context.commit('graph/setOntology', req.ontology)
       context.commit('graph/setStructure', req.structure)
-      context.commit('graph/setStructureTree', buildTree(req.structure))
     }
   }
-}
-
-function buildTree (triples) {
-  const tree = triples
-    .toArray()
-    .reduce((terms, triple) => {
-      if (triple.subject.termType !== 'NamedNode') {
-        return terms
-      }
-
-      try {
-        const iri = triple.subject.value
-        const parts = iri.split('/').slice(3).filter(Boolean)
-        const path = `/${parts.join('/')}`
-
-        let parent = terms
-        while (parts.length) {
-          const child = parts.shift()
-          const idx = parent.children.map(child => child.lvl).indexOf(child)
-          if (idx === -1) {
-            parent.children.push({ iri, path, lvl: child, children: [] })
-            parent = parent.children[parent.children.length - 1]
-          } else {
-            parent = parent.children[idx]
-          }
-        }
-      } catch (err) {
-        console.error(err)
-      }
-      return terms
-    }, { children: [] })
-  return tree
 }
