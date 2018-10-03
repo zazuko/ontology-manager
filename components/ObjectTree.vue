4e5c3a41f0ad99a33589c73caf564c371d062e71
Victor Felder <victor@draft.li>
2018-10-03 13:56:38 +0200

    Ontology side-nav

---
+++ components/ObjectTree.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p class="menu-label">
+      {{ tree.label }}
+    </p>
+    <ul
+      class="menu-list">
+      <object-node
+        v-for="node in tree.children"
+        :key="node.iri"
+        :tree="node"
+        :current-iri="currentIri" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ObjectNode from '~/components/ObjectNode'
+
+export default {
+  name: 'ObjectTree',
+  components: {
+    ObjectNode
+  },
+  props: ['tree', 'item', 'currentIri']
+}
+</script>
