e9429854cbc25ec7224e982c0a06912c29402122
Victor Felder <victor@draft.li>
2018-10-08 16:04:35 +0200

    Request property form

--- pages/proposals/new.vue
+++ pages/proposals/new.vue
@@ -1,9 +1,132 @@
 <template>
-  <section class="container">
-    <button
-      class="button"
-      @click="paf">paf</button>
-  </section>
+  <div>
+
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          Request New Property
+        </h1>
+        <p>
+          This form allows suggesting new elements to include in the ontology.
+        </p>
+        <p>
+          Once submitted, the proposal will be discussed and eventually accepted or rejected by official team members.
+        </p>
+
+        <div class="box">
+          <div class="field">
+            <label class="label">Motivation</label>
+            <div class="columns">
+              <div class="column">
+                <div class="control">
+                  <textarea
+                    class="textarea"
+                    placeholder="" />
+                </div>
+              </div>
+              <div class="column">
+                <p>
+                  In your motivation, please mention involved parties and other supportive
+                  entities, what shortcoming this proposal is expected to overcome
+                  or what purpose it serves.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <label class="label">Suggested Property Name</label>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="text">
+                </div>
+              </div>
+            </div>
+            <div class="column" />
+          </div>
+
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <label class="label">Short Description</label>
+                <div class="control">
+                  <textarea
+                    class="textarea"
+                    placeholder="" />
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="field">
+                <label class="label">Long Description (optional)</label>
+                <div class="control">
+                  <textarea
+                    class="textarea"
+                    placeholder="" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr>
+
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <label class="label">Applies to the Following Classes</label>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="text">
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="field">
+                <label class="label">Expected Type</label>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="text">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="field">
+            <label class="label">Example</label>
+            <div class="control">
+              <textarea
+                class="textarea"
+                placeholder="" />
+            </div>
+          </div>
+        </div>
+
+        <div class="field is-grouped">
+          <p class="control">
+            <button
+              class="button is-primary"
+              @click="createProposal">
+              Submit Proposal
+            </button>
+          </p>
+          <p class="control">
+            <button class="button">
+              Cancel
+            </button>
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -22,7 +145,7 @@ export default {
     await datasetsSetup(this.$store)
   },
   methods: {
-    async paf () {
+    async createProposal () {
       const headers = { headers: { authorization: `Bearer ${this.$apolloHelpers.getToken()}` } }
       try {
         const result = await axios.post('/api/proposals/new', { iri: this.iri }, headers)
