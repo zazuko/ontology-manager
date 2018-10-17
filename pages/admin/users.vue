250428d4beaac351c800b1dfe9b33d8f1fbab8ca
Victor Felder <victor@draft.li>
2018-10-17 12:04:56 +0200

    Admin users promotion/demotion interface

---
+++ pages/admin/users.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="container">
+    <admin-menu />
+
+    <admin-user-list
+      :users="users.nodes"
+      @updated="refetch()" />
+
+  </section>
+</template>
+
+<script>
+import users from '@/apollo/queries/adminUserList'
+import AdminUserList from '@/components/AdminUserList.vue'
+import AdminMenu from '@/components/AdminMenu.vue'
+import { datasetsSetup } from '@/libs/utils'
+
+export default {
+  middleware: 'authenticatedAdmin',
+  components: {
+    AdminUserList,
+    AdminMenu
+  },
+  data: () => ({
+    users: {
+      nodes: []
+    }
+  }),
+  async created () {
+    await datasetsSetup(this.$store)
+  },
+  methods: {
+    refetch () {
+      this.$apollo.queries.users.refetch()
+    }
+  },
+  apollo: {
+    users: {
+      prefetch: true,
+      query: users,
+      variables () {
+        return {
+          threadType: 'PROPOSAL'
+        }
+      },
+      fetchPolicy: 'cache-and-network'
+    }
+  }
+}
+</script>
