8ca7eb8b112b082e11cc4d9407e779b3b92ef64e
Victor Felder <victor@draft.li>
2018-10-18 13:55:56 +0200

    Ontology cache

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const jwt = require('express-jwt')
 const bodyParser = require('body-parser')
 const express = require('express')
+const config = require(require.resolve('../ontology.config.js'))
 
 const app = express()
 
@@ -8,18 +9,27 @@ const app = express()
 Except when linking github oauth token with postgraphile JWT, all
 API requests that come through here need a valid postgraphile JWT
 */
+const filesRoutes = Object.values(config.github.files).map((file) => `/blob/${file}`)
+const unprotectedRoutes = ['', '/link']
+  .concat(filesRoutes)
+  .reduce((routes, route) => {
+    const path = `/api${route}`
+    routes.push(path)
+    // no smart trailing slash handling unfortunately
+    routes.push(`${path}/`)
+    return routes
+  }, [])
+
 app.use(
   jwt({ secret: process.env.POSTGRAPHILE_TOKEN_SECRET })
-    // no smart trailing slash handling unfortunately
-    .unless({ path: ['/api', '/api/', '/api/link', '/api/link/'] }))
+    .unless({ path: unprotectedRoutes }))
+
 app.use(bodyParser.json())
 app.use('/', apiMiddleware())
 
 module.exports = { path: '/api', handler: app }
 
 function apiMiddleware () {
-  const config = require(require.resolve('../ontology.config.js'))
-
   let api
   if (config.github) {
     api = require('./github')
