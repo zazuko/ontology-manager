686039bdcdc5273a8cd07472f5cb3baac96875b7
Victor Felder <victor@draft.li>
2018-10-29 18:48:03 +0100

    Property request: progression

--- pages/proposal/new-property.vue
+++ pages/proposal/new-property.vue
@@ -3,70 +3,115 @@
     <section class="section">
 
       <div class="container">
+        <div class="columns">
+          <div class="column is-3" />
+          <div class="column">
 
-        <h1 class="title">
-          Property Request<span
-            v-show="prop.name">:
-            "{{ prop.name }}"
-          </span>
-        </h1>
-        <h2 class="subtitle">
-          On <code>{{ iri }}</code>
-        </h2>
-        <p>
-          This form allows suggesting new elements to include in the ontology.
-        </p>
-        <p>
-          Once submitted, the proposal will be discussed and eventually accepted or rejected by official team members.
-        </p>
-        <div class="box">
-          <div class="field">
-            <label class="label">Motivation</label>
-            <div class="columns">
-              <div class="column">
-                <div class="control">
-                  <textarea
-                    v-debounce
-                    v-model.lazy="prop.motivation"
-                    class="textarea"
-                    placeholder="" />
-                </div>
-              </div>
-              <div class="column">
-                <p>
-                  In your motivation, please mention involved parties and other supportive
-                  entities, what shortcoming this proposal is expected to overcome
-                  or what purpose it serves.
-                </p>
-              </div>
-            </div>
+            <h1 class="title">
+              Property Request<span
+                v-show="prop.name">:
+                "{{ prop.name }}"
+              </span>
+            </h1>
+            <h2 class="subtitle">
+              On <code>{{ iri }}</code>
+            </h2>
+            <p>
+              This form allows suggesting new elements to include in the ontology.
+            </p>
+            <p>
+              Once submitted, the proposal will be discussed and eventually accepted or rejected by official team members.
+            </p>
+
           </div>
         </div>
 
-        <new-property-form
-          :iri="iri">
+        <div class="columns">
+          <div class="column is-3">
+            <div class="box">
+              <p class="label">
+                New Proposal Progression
+              </p>
+              <!-- <progress
+                class="progress is-small is-success"
+                value="15"
+                max="100">15%</progress> -->
+              <ul class="progression">
+                <!-- loop over a computedProperty here, which contains all the steps -->
+                <li
+                  v-for="(step, index) in progressionSteps"
+                  :key="index"
+                  :class="{ done: step.check() }">
+                  <span
+                    v-if="step.html"
+                    v-html="step.html" />
+                  <span v-else>
+                    {{ step.text }}
+                  </span>
+                  <span v-if="step.path">
+                    <br>
+                    {{ prop[step.path] }}
+                  </span>
+                  <span v-else />
+                </li>
+                <!-- … loop over classChildren and get path to check for steps -->
+              </ul>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box">
+              <div class="field">
+                <label class="label">Motivation</label>
+                <div class="columns">
+                  <div class="column">
+                    <div class="control">
+                      <textarea
+                        v-debounce
+                        v-model.lazy="prop.motivation"
+                        class="textarea"
+                        placeholder="" />
+                    </div>
+                  </div>
+                  <div class="column">
+                    <p>
+                      In your motivation, please mention involved parties and other supportive
+                      entities, what shortcoming this proposal is expected to overcome
+                      or what purpose it serves.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <p v-show="error">{{ error }}</p>
+            <new-property-form
+              :iri="iri">
 
-          <div class="field is-grouped">
-            <p class="control">
-              <button
-                class="button is-primary"
-                @click.prevent="sendProposal">
-                Submit Proposal
-              </button>
-            </p>
-            <p class="control">
-              <button
-                class="button"
-                @click.prevent="clear">
-                Cancel
-              </button>
-            </p>
+              <p v-show="error">{{ error }}</p>
+
+              <div
+                id="submit"
+                class="field is-grouped">
+                <p class="control">
+                  <button
+                    class="button is-primary"
+                    @click.prevent="sendProposal">
+                    Submit Proposal
+                  </button>
+                </p>
+                <p class="control">
+                  <button
+                    class="button"
+                    @click.prevent="clear">
+                    Cancel
+                  </button>
+                </p>
+              </div>
+
+            </new-property-form>
+
           </div>
 
-        </new-property-form>
-
+        </div>
       </div>
 
     </section>
@@ -105,6 +150,29 @@ export default {
     prop () {
       return this.$deepModel('prop.prop')
     },
+    progressionSteps () {
+      const steps = [
+        this.motivationStep(),
+        this.detailsStep()
+      ]
+
+      // const newSteps = this.newSteps()
+      // if (newSteps.length) {
+      //   steps.push(...newSteps)
+      // }
+
+      const lastStep = {
+        check: () => steps.reduce((acc, step, i, col) => {
+          if (i !== col.length - 1) {
+            return acc && step.check()
+          }
+          return acc
+        }, true),
+        html: '<a href="#submit">Finalize and Submit Proposal</a>'
+      }
+      steps.push(lastStep)
+      return steps
+    },
     ...propertyGetters(['success', 'error'])
   },
   watch: {
@@ -122,6 +190,24 @@ export default {
     sendProposal () {
       const token = this.$apolloHelpers.getToken()
       this.submit(token)
+    },
+    newSteps (steps = [], path = 'classChildren') {
+      return this.prop[path].reduce((newSteps, child) => {
+
+      }, steps)
+    },
+    motivationStep (path = '') {
+      return {
+        check: () => this.prop[`${path}motivation`],
+        text: 'Enter a Motivation'
+      }
+    },
+    detailsStep (path = '') {
+      return {
+        check: () => this.prop[`${path}label`] && this.prop[`${path}comment`],
+        text: 'Enter New Property Details',
+        path: 'label'
+      }
     }
   },
   validate ({ query }) {
