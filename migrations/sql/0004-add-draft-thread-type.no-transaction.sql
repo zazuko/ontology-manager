ba4c05d70deb7fcd90e605e1464bbc4ac2ecbf3c
Victor Felder <victor@draft.li>
2018-10-31 11:21:11 +0100

    Add thread type and proposal object; make migration system run some migrations outside of transactions

---
+++ migrations/sql/0004-add-draft-thread-type.no-transaction.sql
@@ -0,0 +1,2 @@
+-- add 'draft' type to save proposals before submitting them
+alter type ontology_editor.status add value 'draft';
---
+++ migrations/sql/0005-proposal-content.sql
@@ -0,0 +1,2 @@
+alter table ontology_editor.thread add column proposal_object json default null;
+alter table ontology_editor.thread add column branch_name text default null;
