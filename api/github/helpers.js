938c49903e1a4cecab2283b959b9bf58fda65946
Victor Felder <victor@draft.li>
2018-11-27 10:49:52 +0100

    Upgrade dependencies

--- api/github/helpers.js
+++ api/github/helpers.js
@@ -13,7 +13,7 @@ function helpersFactory (octokit) {
   }
 
   async function getFileSHA ({ owner, repo, path, ref } = {}) {
-    const result = await octokit.repos.getContent({ owner, repo, path, ref })
+    const result = await octokit.repos.getContents({ owner, repo, path, ref })
 
     return result.data.sha
   }
