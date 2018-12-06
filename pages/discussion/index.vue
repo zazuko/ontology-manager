2787292df3f22ab1d6a2e1c610c8159e58fd3349
Victor Felder <victor@draft.li>
2018-12-06 16:50:02 +0100

    Add a TODO to unused? page

--- pages/discussion/index.vue
+++ pages/discussion/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <section class="section">
+      <!--
+      TODO: why this placeholder URL query? is this index page used at all?
+      It's supposed to be a public list of all discussion threads, I don't think
+      it was planned…
+      -->
       <nuxt-link
         :to="{ name: 'discussion-new', query: { iri: 'http://example.org/foo' } }"
         class="button is-link">
