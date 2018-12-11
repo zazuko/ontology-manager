625bcd7d809abe434a3df2469ba839dbd3dd6e7d
abdelaz3r <gil.clavien@gmail.com>
2018-12-11 14:28:17 +0100

    Fix: add component for previous commit

---
+++ components/layout/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <div class="container level">
+      <div class="level-left">
+        <nuxt-link
+          :to="{ name: 'index', params: {} }"">
+          <img
+            class="footer-icon"
+            src="~/assets/images/dcf-logo.svg">
+        </nuxt-link>
+         @ 2018 DCF. All rights reserved
+      </div>
+
+      <div class="level-right">
+        Back to the top
+        <span class="footer-back-icon icon is-medium">
+          <i class="mdi mdi-chevron-up" />
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+    //
+  }
+}
+</script>
