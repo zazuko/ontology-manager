41f82037104b8abb403c483d2103946705da53c6
Victor Felder <victor@draft.li>
2018-12-13 12:10:12 +0100

    Add link to forge in admin proposal list

---
+++ components/admin/ForgeLink.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <a :href="id | forgeLink">
+    <span
+      class="icon is-small"
+      :title="title">
+      <i class="mdi mdi-open-in-new" />
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ForgeLink',
+  props: {
+    id: {
+      type: [Number, String],
+      required: true
+    }
+  },
+  computed: {
+    title () {
+      // TODO: make this forge-agnostic
+      return 'See on GitHub'
+    }
+  }
+}
+</script>
