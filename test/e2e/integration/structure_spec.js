945e0cf537b8041e530633d2969892dcb6577823
Victor Felder <victor@draft.li>
2019-01-16 18:03:18 +0100

    tests: e2e navigation without page load

---
+++ test/e2e/integration/structure_spec.js
@@ -0,0 +1,40 @@
+/* global cy */
+const snapshotBoxes = (boxes) => {
+  const boxesTitles = Cypress._
+    .chain(boxes)
+    .map('textContent')
+    .map((x) => x.trim())
+    .value()
+  cy.wrap(boxesTitles).snapshot()
+}
+
+describe('Structure', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  it('Shows all pouches on homepage', () => {
+    cy.goto('/')
+    cy.get('.class-box-title').then(snapshotBoxes)
+  })
+
+  it('Nests properly', () => {
+    cy.goto('/letsnest/container/').wait(200)
+    cy.get('.main-title').should('be', 'Container')
+    cy.get('.class-box-title').then(snapshotBoxes)
+    cy.goto('/letsnest/container/container/').wait(200)
+    cy.get('.main-title').should('be', 'Deeper Container')
+    cy.get('.class-box-title').then(snapshotBoxes)
+  })
+
+  it('Loads a pouch', () => {
+    cy.goto('/pouch/TrackUpdatePouch').wait(200)
+    cy.get('.main-title').should('be', 'Track Update Pouch')
+    cy.get('.class-box-title').then(snapshotBoxes)
+  })
+
+  it('Navigates to a pouch', () => {
+    cy.visit('/pouch/TrackUpdatePouch')
+    cy.get('.class-box-title').then(snapshotBoxes)
+  })
+})
