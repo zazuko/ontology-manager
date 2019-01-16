fc0c57dbbf87a15004394156e8d4888c88a639d7
Victor Felder <victor@draft.li>
2019-01-16 18:03:18 +0100

    tests: e2e navigation without page load

--- test/e2e/integration/admin/user_spec.js
+++ test/e2e/integration/admin/user_spec.js
@@ -1,8 +1,7 @@
 /* global cy */
 describe('Admin Users List', () => {
   beforeEach(() => {
-    cy.visit('/').login()
-    cy.visit('/admin/users')
+    cy.visit('/').login('/admin/users')
   })
   it('should see myself in the list', () => {
     cy.contains('.admin-table', 'e2e test user')
