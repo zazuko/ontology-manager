99e396eac2e7712efb14cc528925e671669e082d
Victor Felder <victor@draft.li>
2019-01-17 14:09:38 +0100

    feat: display editor version in about page

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container layout-objects-list">
+    <section class="container layout-objects-list-head">
+      <h1 class="main-title">
+        ontology-editor
+      </h1>
+      <h2 class="subtitle">
+        RDF ontology/schema editor
+      </h2>
+
+      <div class="columns">
+        <div class="column">
+          <p>Zazuko Ontology Editor v{{ version }}</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { version } from '@/package.json'
+
+export default {
+  layout: 'background',
+  components: {},
+  data () {
+    return {
+      version
+    }
+  }
+}
+</script>
