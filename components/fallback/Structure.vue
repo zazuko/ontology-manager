0bbddc5d1e7e22fe0e8824b4804eaf60a5faef6e
Victor Felder <victor@draft.li>
2019-01-21 11:09:22 +0100

    fix: object tiles should never grow fullwidth

--- components/fallback/Structure.vue
+++ components/fallback/Structure.vue
@@ -17,15 +17,14 @@
               <div
                 v-for="child in group"
                 :key="child.path"
-                class="tile is-parent">
+                class="tile is-parent object-tile">
                 <pouch-box
                   :label="child.label"
                   :to="{ path: child.path, params: {} }"
                   :iri="child.iri"
                   :properties-count="_get(child, 'properties.length', 0)"
                   :modified="child.modified"
-                  :type="child.type"
-                  class="" />
+                  :type="child.type" />
               </div>
             </div>
           </div>
