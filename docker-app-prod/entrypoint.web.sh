609005b3c15efc77f42408829f24006d49f5920f
Victor Felder <victor@draft.li>
2019-01-31 01:21:09 +0100

    feat: rework deployment, serve static assets with nginx

--- docker-app-prod/entrypoint.web.sh
+++ docker-app-prod/entrypoint.web.sh
@@ -4,7 +4,28 @@ set -euo pipefail
 node setup/migrate.js
 
 if [ ! -f .built ]; then
-  npm run build -- --modern=server && touch .built
+  npm run build -- --modern=server
+
+  touch .built
 fi
 
-npm run start -- --modern=server
+#!/bin/bash
+wait_for_pids()
+{
+    for (( i = 1; i <= $#; i++ )) do
+        wait -n $@
+        status=$?
+        echo "received status: "$status
+        if [ $status -ne 0 ] && [ $status -ne 127 ]; then
+            exit 1
+        fi
+    done
+}
+
+npm run start &
+pid1=$!
+
+nginx -g 'daemon off;' &
+pid2=$!
+
+wait_for_pids $pid2 $pid1
