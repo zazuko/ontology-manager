bec0ccd85098cdbbb9608e56b2297d03826d8c18
Victor Felder <victor@draft.li>
2019-02-26 11:52:07 +0100

    fix: add zazuko logo

--- plugins/nuxt-init.client.js
+++ plugins/nuxt-init.client.js
@@ -1,15 +1,5 @@
 export default async (context) => {
-  if (process.client || process.browser) {
-    Object.entries(context.env).forEach(([key, val]) => {
-      process.env[key] = val
-    })
-  }
   if (process.browser) {
-    await new Promise((resolve) => {
-      setTimeout(async () => {
-        await context.store.dispatch('nuxtClientInit', context)
-        resolve()
-      }, 0)
-    })
+    await context.store.dispatch('nuxtClientInit', context)
   }
 }
