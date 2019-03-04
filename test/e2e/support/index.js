c9d27918a286321443a7aed40ac5717f52271097
Victor Felder <victor@draft.li>
2019-03-04 11:36:47 +0100

    test(e2e): fix app setup

--- test/e2e/support/index.js
+++ test/e2e/support/index.js
@@ -2,3 +2,12 @@ import snapshot from '@cypress/snapshot'
 import './commands'
 
 snapshot.register()
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  if (err) {
+    console.error(err)
+  }
+  return false
+})
