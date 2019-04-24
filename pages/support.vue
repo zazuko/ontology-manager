c2882e6bd77a661bd09f9e44e835d8073baf8c05
Victor Felder <victor@draft.li>
2019-04-24 15:06:45 +0200

    feat: add support and sample gh issue templates

---
+++ pages/support.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container layout-objects-list">
+    <section class="container layout-objects-list-head has-text-left">
+      <h1 class="main-title has-text-left">
+        Support
+      </h1>
+      <h2 class="subtitle has-text-left">
+        Report Bugs / Get Help
+      </h2>
+
+      <div class="columns">
+        <div class="column content">
+          <p>
+            <a
+              :href="bug"
+              class="button is-warning">
+              Report a Bug or Issue
+            </a>
+          </p>
+          <p>
+            <a
+              :href="question"
+              class="button is-success">
+              Ask a Question
+            </a>
+          </p>
+          <p>
+            <a
+              :href="issues"
+              class="button is-info">
+              See all support request
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { version } from '@/package.json'
+
+export default {
+  layout: 'background',
+  components: {},
+  computed: {
+    issues () {
+      return 'https://github.com/' + [
+        this.$store.state.config.editor.github.owner,
+        this.$store.state.config.editor.github.repo,
+        'issues'
+      ].join('/')
+    },
+    bug () {
+      return 'https://github.com/' + [
+        this.$store.state.config.editor.github.owner,
+        this.$store.state.config.editor.github.repo,
+        'issues',
+        'new?template=bug_report.md'
+      ].join('/')
+    },
+    question () {
+      return 'https://github.com/' + [
+        this.$store.state.config.editor.github.owner,
+        this.$store.state.config.editor.github.repo,
+        'issues',
+        'new?template=question.md'
+      ].join('/')
+    }
+  },
+  data () {
+    return {
+      version
+    }
+  }
+}
+</script>
