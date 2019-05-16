22830552d2300d6b01fe1ef35c246a9a11db95a9
Victor Felder <victor@draft.li>
2019-05-16 13:44:46 +0200

    fix(admin): smarter pagination

--- components/layout/Pagination.vue
+++ components/layout/Pagination.vue
@@ -19,7 +19,7 @@
     </nuxt-link>
     <ul class="pagination-list">
       <li
-        v-for="p in range1"
+        v-for="p in paginate[0]"
         :key="p">
         <nuxt-link
           :to="{ name: route, query: { page: p } }"
@@ -29,12 +29,12 @@
           {{ p }}
         </nuxt-link>
       </li>
-      <!-- <li
-        v-show="range2.length">
+      <li
+        v-show="paginate[1].length">
         <span class="pagination-ellipsis">&hellip;</span>
       </li>
       <li
-        v-for="p in range2"
+        v-for="p in paginate[1]"
         :key="p + 1000">
         <nuxt-link
           :to="{ name: route, query: { page: p } }"
@@ -45,11 +45,11 @@
         </nuxt-link>
       </li>
       <li
-        v-show="range3.length">
+        v-show="paginate[2].length">
         <span class="pagination-ellipsis">&hellip;</span>
       </li>
       <li
-        v-for="p in range3"
+        v-for="p in paginate[2]"
         :key="p + 10000">
         <nuxt-link
           :to="{ name: route, query: { page: p } }"
@@ -58,14 +58,12 @@
           :aria-label="label(p)">
           {{ p }}
         </nuxt-link>
-      </li> -->
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
-import _range from 'lodash/range'
-
 export default {
   name: 'Pagination',
   props: {
@@ -90,8 +88,34 @@ export default {
     pagesCount () {
       return Math.ceil(this.resultsCount / this.resultsPerPage)
     },
-    range1 () {
-      return _range(1, this.pagesCount + 1)
+    paginate () {
+      const current = this.page
+      const max = this.pagesCount
+      const pages = [1, current - 1, current, current + 1, max]
+        // clamp to range 1 - max
+        .filter(page => page > 0 && page <= max)
+
+      // split into blocks
+      const blocks = Array.from(new Set(pages))
+        .reduce((blocks, page) => {
+          const currentBlock = blocks.slice(-1)[0]
+
+          if (currentBlock && currentBlock.slice(-1)[0] === page - 1) {
+            currentBlock.push(page)
+          }
+          else {
+            blocks.push([page])
+          }
+
+          return blocks
+        }, [])
+
+      if (blocks.length === 3) {
+        return blocks
+      }
+
+      const [block1, block3 = []] = blocks
+      return [block1, [], block3]
     }
   },
   methods: {
