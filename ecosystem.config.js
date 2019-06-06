f9d8e2e35520d3ba62e771e6a254fdddbd39be65
Victor Felder <victor@draft.li>
2019-06-06 10:35:57 +0200

    fix: adapt max memory restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '250M',
+    max_memory_restart: '300M',
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'production'
