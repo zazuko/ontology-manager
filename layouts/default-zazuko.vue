5792402e24d955ce740438408a5334b3e9d47671
Victor Felder <victor@draft.li>
2019-08-21 15:10:30 +0200

    fix(design): add zazuko ci layouts

---
+++ layouts/default-zazuko.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="has-primary-background">
+    <top-nav />
+    <nuxt />
+    <page-footer />
+  </div>
+</template>
+
+<script>
+import TopNav from '@/components/layout/TopNav'
+import PageFooter from '@/components/layout/PageFooter'
+
+export default {
+  components: {
+    TopNav,
+    PageFooter
+  },
+  methods: {},
+  beforeCreate () {
+    import('@/assets/scss/zazuko-style.scss')
+  },
+  mounted () {
+    if (window.Cypress) {
+      window.appReady = true
+    }
+  }
+}
+</script>
