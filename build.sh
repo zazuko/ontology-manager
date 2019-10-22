ca0e9c56de689d0a35cbe998ad098a70d3020975
Victor Felder <victor@draft.li>
2019-10-22 14:38:44 +0200

    fix(docker): fix build script path

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 DESIGNS=$(ls ./assets/scss/*.scss | sed s%./assets/scss/%% | sed s%-style.scss%%)
+mkdir ./artifacts
 
 for DESIGN in $DESIGNS; do
+  echo "BUILDING $DESIGN"
   # modify nuxt.config.js
   sed -i.bak s%zazuko-style%$DESIGN-style% nuxt.config.js
 
@@ -11,11 +13,12 @@ for DESIGN in $DESIGNS; do
   # delete source maps for client
   rm .nuxt/dist/client/*.map
 
-  mv .nuxt .nuxt_$DESIGN
+  cp -r .nuxt artifacts/nuxt_$DESIGN
 
   # restore nuxt.config.js
   cat nuxt.config.js.bak > nuxt.config.js
   echo "BUILT $DESIGN"
+  ls ./artifacts
 done
 
 rm nuxt.config.js.bak
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ EDITOR_STYLE="${EDITOR_STYLE:-$DEFAULT_DESIGN}"
 
 node setup/migrate
 echo "STARTING EDITOR WITH DESIGN $EDITOR_STYLE"
-ln -s .nuxt_$EDITOR_STYLE .nuxt_original
+cp -r artifacts/nuxt_$EDITOR_STYLE nuxt_original
 node setup/replace-vars
 exec npm start
