10b270dba53741acc30e633f786386e5bdbeb48f
Victor Felder <victor@draft.li>
2019-12-19 12:16:01 +0100

    fix(trifid): serve 406 on unserializable Accepts

--- trifid/passthrough-plugin.js
+++ trifid/passthrough-plugin.js
@@ -1,16 +1,29 @@
+const serializersFor = [
+  'application/ld+json',
+  'application/n-triples',
+  'text/n3',
+  'text/turtle',
+  'application/json'
+]
+
 module.exports = function handler (router) {
   router.use((req, res, next) => {
     if ('iri' in req) {
       req._iri = req.iri
     }
+    const accepted = req.get('Accept') || ''
     if (req.accepts(['html', 'xhtml+xml', 'xml'])) {
-      const accepted = req.get('Accept') || []
       if (accepted.includes('*/*')) {
         const filtered = accepted.split(',').filter(str => !str.includes('*/*')).join(',')
         // need to use lowercase key!
         req.headers.accept = filtered
       }
+      // disables trifid serialization because this iri won't be found in the datasets
       req.iri = '… nothing here …'
+    }
+    else if (!req.accepts(serializersFor)) {
+      res.status(406).send(`Cannot serialize to: ${accepted}`)
+      return
     }
 
     next()
